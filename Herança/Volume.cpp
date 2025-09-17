7f5f4e51192625638ef7bcddd2312e90fd8d4481
guilhermeggloor <guilhermegloor8@gmail.com>
2025-09-17 15:29:46 -0400

    feat: Exercicio de volume herança

---
+++ Herança/Volume.cpp
@@ -0,0 +1,26 @@
+//Crie um aplicativo, em C++, para a representação de sólidos em R3 e 
+//calculo de área de superfície e volume destes sólidos. 
+//Na hierarquia de classes, obrigatoriamente deverá existir uma classe responsável 
+//pelo armazenamento do conjunto de objetos, que representam os sólidos, 
+//e calculo, de áreae volume, total desse conjunto de objetos.
+//Seu aplicativo deve admitir que o usuário realize a inserção de esferas e cilindros, 
+//requisitar o calculo da área e volume total dos sólidos inseridos. 
+//Além disso deve prever a ampliação do conjunto de sólidos, implementados inicialmente, 
+//sem a necessidade de alteração das classes criadas inicialmente
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Solido {
+
+};
+
+
+int main()
+{
+    cout << "Hello World !" << endl;
+
+    return 0;
+}
